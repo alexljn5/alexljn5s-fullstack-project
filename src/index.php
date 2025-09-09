05553fa3789eada7074af3a104437073a3115983
alexljn5 <alexljn5@gmail.com>
2025-09-09 18:26:09 +0200

    neat

--- src/index.php
+++ src/index.php
@@ -1,11 +1,53 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Hello World!</title>
+    <link href="styles.css" rel="stylesheet">
 </head>
+
 <body>
-    
+    <div class="header-container">
+        <header>
+            <div class="logo-box">
+                <img src="img/logoplaceholder.webp" alt="Placeholder Logo">
+            </div>
+            <div class="date-and-time-box">
+                <p2>Date and Time.</p2>
+            </div>
+            <div class="weather-box">
+                <p3>Weather.</p3>
+            </div>
+        </header>
+    </div>
+
+    <div class="input-container">
+        <div class="welcome-box">
+            <p>Welcome [Insert User Here Later]!</p>
+        </div>
+
+        <div class="input-boxes">
+            Username/Email:
+            <input type="text" placeholder="Username/Email">
+            Password:
+            <input type="text" placeholder="Password">
+        </div>
+    </div>
+
+    <div class="footer-container">
+        <footer>
+            <div class="business-number-box">
+                <p4>business number.</p4>
+            </div>
+            <div class="legal-text-box">
+                <p5>Legal text.</p5>
+            </div>
+            <div class="copyright-box">
+                <p6>Copyright.</p6>
+        </footer>
+    </div>
 </body>
+
 </html>
