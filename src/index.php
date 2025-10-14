10feb9a88aae53d8c885f1e6008d5a7813cfc855
alexljn5 <alexljn5@gmail.com>
2025-10-14 10:12:55 +0200

    refactoring

--- src/index.php
+++ src/index.php
@@ -1,5 +1,5 @@
 <?php
-session_start(); // Start session for login status
+session_start();
 ?>
 
 <!DOCTYPE html>
@@ -8,12 +8,25 @@ session_start(); // Start session for login status
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Tools4Ever Employee Login</title>
+    <title>Tools4Ever Employee Portal</title>
     <link href="styles.css" rel="stylesheet">
+    <script src="javascript/headerui.js"></script>
 </head>
 
 <body>
-    <?php include 'php/header.php'; ?>
+    <div class="header-container">
+        <header>
+            <div class="logo-box">
+                <img src="img/logoplaceholder.webp" alt="Tools4Ever Logo">
+            </div>
+            <div class="date-and-time-box">
+                <p>Date and Time</p>
+            </div>
+            <div class="weather-box">
+                <p>Weather</p>
+            </div>
+        </header>
+    </div>
     <div class="input-container">
         <div class="welcome-box">
             <p>Welcome to the Tools4Ever Employee Portal!</p>
@@ -24,14 +37,26 @@ session_start(); // Start session for login status
         <form action="login.php" method="POST">
             <div class="input-boxes">
                 <label>Username:</label>
-                <input type="text" name="username" placeholder="Username" required>
+                <input type="text" name="username" placeholder="Username/Email" required>
                 <label>Password:</label>
                 <input type="password" name="password" placeholder="Password" required>
                 <button type="submit">Login</button>
             </div>
         </form>
     </div>
-    <?php include 'php/footer.php'; ?>
+    <div class="footer-container">
+        <footer>
+            <div class="business-number-box">
+                <p>Business Number</p>
+            </div>
+            <div class="legal-text-box">
+                <p>Legal Text</p>
+            </div>
+            <div class="copyright-box">
+                <p>Copyright</p>
+            </div>
+        </footer>
+    </div>
 </body>
 
 </html>
